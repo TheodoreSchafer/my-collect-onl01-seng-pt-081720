9ddad3bb1fef9dc3a8199970513b9463384adc0c
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-25 16:44:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,18 +1,12 @@
-
+require 'pry'
 
 def my_collect(collection)
   i = 0
-  empty_array = []
-  while i < empty_array.length
+  new_array = []
+  while i < collection.length
     binding.pry
-    yield empty_array[i]
-    i = i + 1
+    yield (new_array[i])
+    i += 1
   end
-  return empty_array
-end
-
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-
-my_collect(array) do |name|
-  name.split(" ").first
+  return new_array
 end
