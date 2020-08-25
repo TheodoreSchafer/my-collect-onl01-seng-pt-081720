29f7df3f8cd5804ea4795e0ce30e65b6463e1a2d
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-25 16:45:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 def my_collect(collection)
   i = 0
-  
+
   while i < collection.length
 
     yield (collection[i])
     i += 1
   end
-  return new_array
+  return collection
 end
